4d79c367a8b256803dc941d694756c5a36623ef0
juanlopezmarin <Juanteniente45@gmail.com>
2025-05-08 14:48:35 +0200

    falta añadir base de dtaos

--- app/src/main/java/com/example/relojfichajeskairos24h/URLs.kt
+++ app/src/main/java/com/example/relojfichajeskairos24h/URLs.kt
@@ -4,7 +4,7 @@ object BuildURL {
     const val HOST = "https://setfichaje.kairos24h.es/"
     const val DEMO = "https://demosetfichaje.kairos24h.es/"
     const val ACTION = "index.php?r=citaRedWeb/crearFichajeExterno"
-    const val PARAMS = "&xEntidad=1002" +
+    const val PARAMS = "&xEntidad=1005" +
                         "&cKiosko=TABLET1" +
                         "&cEmpCppExt=" +
                         "&cTipFic=" +
