ebe5d3e8fa99f7b24b2ae3c0731030fdefa144ac
juanlopezmarin <Juanteniente45@gmail.com>
2025-05-20 08:35:50 +0200

    app relojKairos24h Tragabuche acabada1 Hay que tener cuidado con las redimeniones

--- app/src/main/java/com/example/relojfichajeskairos24h/URLs.kt
+++ app/src/main/java/com/example/relojfichajeskairos24h/URLs.kt
@@ -1,9 +1,9 @@
 package com.example.relojfichajeskairos24h
 
 object BuildURL {
-    const val HOST = "https://setfichaje.kairos24h.es/"
+    const val HOST = "https://rincontragabuche.kairos24h.es/"
     const val ACTION = "index.php?r=citaRedWeb/crearFichajeExterno"
-    const val PARAMS = "&xEntidad=1007" +
+    const val PARAMS = "&xEntidad=1005" +
                         "&cKiosko=TABLET1" +
                         "&cEmpCppExt=" +
                         "&cTipFic=" +
@@ -30,7 +30,7 @@ object BuildURL {
  */
 object Imagenes {
     // Nombres de recursos en drawable
-    const val LOGO_CLIENTE = "logo_rfag"
+    const val LOGO_CLIENTE = "logo_tragabuche"
     const val LOGO_DESARROLLADORA = "logo_desarrolladora"
 
     data class PropiedadesImagen(
